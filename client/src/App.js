e209870c8bd2a2d6904d2d6ee8e47b0ec5f8cff9
DILIP SANAP <dilipinbsanap@gmail.com>
2023-10-17 17:37:08 +0530

    Carousel and banner created successfully

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,16 @@ import {BrowserRouter} from 'react-router-dom'
 
 // components
 import Header from './components/Header/Header'
+import Home from './components/Home/Home';
+import { Box } from '@mui/material';
+
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
+      <Box style={{marginTop:54}}>
+        <Home />
+      </Box>
     </BrowserRouter>
   );
 }
